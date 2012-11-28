dcb9b83c823dc247c9732e2e1e3f4b6f9023e7b7
nessia.gp <nessia.gp@3a031483-e89a-2438-14a8-ae54a014a3c2>
2012-11-28 19:24:24 +0000

    Bean Recibo

--- trunk/AtencionAoCidadan/src/java/model/RecibosEstados.java
+++ trunk/AtencionAoCidadan/src/java/model/RecibosEstados.java
@@ -5,15 +5,21 @@ package model;
  * @author nessa
  */
 public enum RecibosEstados {
-    Pendente(0), Pagado(1), Anulado(2) ;
+    Pendente(0, "Pendete"), Pagado(1,"Pagado"), Anulado(2,"Anulado") ;
 
     private final int value;
-    private RecibosEstados(int value) {
+    private final String nombre;
+    private RecibosEstados(int value, String nombre) {
         this.value = value;
+        this.nombre = nombre;
     }
 
     public int getValue() {
         return value;
+    }
+    
+    public String getNombre(){
+        return nombre;
     }
     
 }
