82e01ea4f4dffee9d9bc1bbd9403b98ab4656804
nessia.gp <nessia.gp@3a031483-e89a-2438-14a8-ae54a014a3c2>
2012-12-02 21:54:50 +0000

    Domiciliacion de objetos tributarios que devenguen recibos de forma periodica

--- AtencionAoCidadan/src/java/model/CuentaBancaria.java
+++ AtencionAoCidadan/src/java/model/CuentaBancaria.java
@@ -1,13 +1,86 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
+
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 
 /**
  *
  * @author nessa
  */
-public class CuentaBancaria {
+@Entity
+public class CuentaBancaria implements Serializable {
+ 
+    /* Atributos */
+    
+    @Id
+    @GeneratedValue
+    private Long id;
+    @Column(unique = true)
+    private String codigo;
+    private String descripcion;
+    private String banco;
+    
+    @ManyToMany
+    List<Ciudadano> titulares;
+    
+    /* Constructores */
+    public CuentaBancaria(){
+        titulares = new ArrayList<Ciudadano>();
+    }
+    
+    /* Getters & setters */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCodigo() {
+        return codigo;
+    }
+
+    public void setCodigo(String codigo) {
+        this.codigo = codigo.replaceAll(" ", "");
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getBanco() {
+        return banco;
+    }
+
+    public void setBanco(String banco) {
+        this.banco = banco;
+    }
+
+    public List<Ciudadano> getTitulares() {
+        return titulares;
+    }
+
+    public void setTitulares(List<Ciudadano> titulares) {
+        this.titulares = titulares;
+    }
+    
+    public boolean validate(){
+        //return codigo!= null && !codigo.isEmpty() && codigo.length()==20;
+        return true;
+    }
+    
+    
     
 }
--- AtencionAoCidadan/src/java/model/Domiciliacion.java
+++ AtencionAoCidadan/src/java/model/Domiciliacion.java
@@ -1,13 +1,88 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
 package model;
+
+import java.io.Serializable;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 /**
  *
  * @author nessa
  */
-public class Domiciliacion {
+@Entity
+public class Domiciliacion implements Serializable {
+    /* Atributos */
+    @Id
+    @GeneratedValue
+    private Long id;
+    private String referencia;
+    private boolean esAutoliquidacion = false;
+    @ManyToOne 
+    private RecibosCategoria categoria;
+    @ManyToOne
+    private Ciudadano ciudadano;
+    @ManyToOne
+    private CuentaBancaria cuenta;
+    
+    
+    /* Constructores */
+    public Domiciliacion(){}
+    
+    /* Getters & setters */
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getReferencia() {
+        return referencia;
+    }
+
+    public void setReferencia(String referencia) {
+        this.referencia = referencia;
+    }
+
+    public boolean isEsAutoliquidacion() {
+        return esAutoliquidacion;
+    }
+
+    public void setEsAutoliquidacion(boolean esAutoliquidacion) {
+        this.esAutoliquidacion = esAutoliquidacion;
+    }
+
+    public RecibosCategoria getCategoria() {
+        return categoria;
+    }
+
+    public void setCategoria(RecibosCategoria categoria) {
+        this.categoria = categoria;
+    }
+
+    public Ciudadano getCiudadano() {
+        return ciudadano;
+    }
+
+    public void setCiudadano(Ciudadano ciudadano) {
+        this.ciudadano = ciudadano;
+    }
+
+    public CuentaBancaria getCuenta() {
+        return cuenta;
+    }
+
+    public void setCuenta(CuentaBancaria cuenta) {
+        this.cuenta = cuenta;
+    }
+    
+    // TODO: hacer función
+    public boolean validate(){
+        return true;
+    }
+    
     
 }
