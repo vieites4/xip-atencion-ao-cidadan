63d12a5ea90a2c10daf3cded44ffe9423f1b1551
nessia.gp <nessia.gp@3a031483-e89a-2438-14a8-ae54a014a3c2>
2012-12-02 22:16:53 +0000

    ejemplos sql

--- AtencionAoCidadan/sql/ejemplos.sql
+++ AtencionAoCidadan/sql/ejemplos.sql
@@ -1,3 +1,12 @@
+INSERT INTO usuario(
+            id, email, movil, tipo, usuario)
+    VALUES (124, 'paquito@mail.com', '645546456', 2, 'flogar');
+
+INSERT INTO ciudadano(
+            id, apellidos, direccion, dni, nombre, nivelinstruccion, sexo, 
+            designacion, telefono, usuario_id, cp)
+    VALUES (124, 'Lopez García', 'Calle de la Rosa','12345678Z', 'Franciso', 'Bachiller', 'M', 
+            'Paquito', '987897987', 124, '19879');
 
 INSERT INTO reciboscategoria(id, abreviatura, nombre, referencia)
 VALUES 
@@ -9,5 +18,5 @@ INSERT INTO recibo(
             id, ano, dataaprobacion, datacobro, datalimitepagamento, descripcion, 
             esautoliquidacion, estado, importe, numerorecibo, referencia, 
             tipo, categoria_id, ciudadano_id)
-VALUES (1, 2010, '2010-10-01 10:10', '2010-10-01 10:10', '2010-10-01 10:10', '', 
-            false, 1, 10.0, 1, '234s234', 'rb', 1, 24);
+VALUES (124, 2010, '2010-10-01 10:10', '2010-10-01 10:10', '2010-10-01 10:10', '', 
+            false, 1, 10.0, 1, '0893hvp', 'rb', 2, 124);
